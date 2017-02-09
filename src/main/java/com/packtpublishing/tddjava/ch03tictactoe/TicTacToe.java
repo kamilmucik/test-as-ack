26cc6e4491042ec564bf43ca28d7584bd6a34827
Kamil Mucik <kamil.mucik@eo.pl>
2017-02-09 12:10:31 +0100

    eliminate DTO error

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,21 +1,13 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
-
-import org.apache.log4j.Logger;
 import pl.estrix.model.test.TestDto;
 
 class TicTacToe {
 
-    final static Logger logger = Logger.getLogger(TicTacToe.class);
-
     TestDto print(){
-        TestDto dto = null;
-
-        dto = TestDto
+        return TestDto
                 .builder()
                 .name("hello")
                 .build();
-
-        return dto;
     }
 }
