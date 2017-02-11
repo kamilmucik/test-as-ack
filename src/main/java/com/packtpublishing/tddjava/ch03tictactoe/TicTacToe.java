7426ab51645422f56a7d556ddaa7e4300ff77bcd
Kamil <k.mucik@e-strix.pl>
2017-02-11 12:53:02 +0100

    test for chapter 3 issue 4

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -15,31 +15,38 @@ class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+        if (isWin(x, y)) {
             return "Wygral " + lastPlayer;
-        }
-        return "Brak zwyciezcy";
+        } else if (isDraw()){
+            return "Wynik remisowy";
+        }else return "Brak zwyciezcy";
     }
 
-    private boolean isWin() {
-        int playerTotal = lastPlayer * SIZE;
-        char diagonal1 = '\0';
-        char diagonal2 = '\0';
-        for (int i = 0; i< SIZE; i++) {
-            diagonal1 += board[i][i];
-            diagonal2 += board[i][SIZE - i - 1];
-            if (board[0][i] + board[1][i] + board[2][i] == playerTotal){
-                return true;
-            } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal ) {
-                return true;
+    private boolean isDraw() {
+        for (int x=0; x < SIZE; x++){
+            for (int y=0; y < SIZE; y++){
+                if (board[x][y] == '\0'){
+                    return false;
+                }
             }
         }
-
-        return diagonal1 == playerTotal || diagonal2 == playerTotal;
-
+        return true;
     }
 
-    private void checkAxis(int axis) throws Throwable {
+    private boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal= vertical= diagonal1= diagonal2 = '\0';
+        for (int i = 0; i< SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
+        }
+        return diagonal1 == playerTotal || diagonal2 == playerTotal || horizontal == playerTotal || vertical == playerTotal;
+    }
+
+    private void checkAxis(int axis) throws Exception {
         if (axis < 1 || axis > 3) {
             throw new OwnRuntimeException("Wartosc X wykracza poza plansze");
         }
