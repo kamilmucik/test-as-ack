fe0a05857c727c549a5a916ae6868f942810dd4c
Kamil Mucik <kamil.mucik@eo.pl>
2017-02-21 13:19:40 +0100

    Added some tests

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,13 +2,27 @@ package com.packtpublishing.tddjava.ch04ship;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 class Ship {
 
 	private Location location;
+	private Point max;
+	private List<Point> obstacles;
 
 	Ship(Location location) {
 		this.location = location;
+		this.max = new Point(50, 50);
+		this.obstacles = new ArrayList<>();
 	}
 
+	boolean moveForward() {
+		return this.location.forward(max, obstacles);
+	}
+
+	boolean moveBackward() {
+		return this.location.backward(max, obstacles);
+	}
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -1,16 +1,56 @@
 package com.packtpublishing.tddjava.ch04ship;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShipSpec {
 
+	private Point max;
+	private List<Point> obstacles;
+	private Location location;
+	private Ship ship;
+
+	@Before
+	public void beforeTest() {
+		location = new Location(new Point(21, 13), Direction.NORTH);
+		ship = new Ship(location);
+		max = new Point(50, 50);
+		obstacles = new ArrayList<>();
+	}
+
 	@Test
 	public void whenInstantiatedThenLocationIsSet() {
-		Location location = new Location(new Point(21, 13), Direction.NORTH);
-		Ship ship = new Ship(location);
-		
 		assertThat(ship.getLocation()).isEqualTo(location);
+	}
+
+	@Test
+	public void givenNorthWhenMoveForwardThenYDecreases() {
+		Location expected = location.copy();
+		expected.forward(max, obstacles);
+		ship.moveForward();
+
+		assertThat(ship.getLocation()).isEqualTo(expected);
+	}
+
+	@Test
+	public void whenMoveBackwardThenBackward() {
+		Location expected = location.copy();
+		expected.backward(max, obstacles);
+		ship.moveBackward();
+
+		assertThat(ship.getLocation()).isEqualTo(expected);
+	}
+
+	@Test
+	public void givenEastWhenMoveForwardThenXIncreases() {
+		ship.getLocation().setDirection(Direction.EAST);
+		ship.moveForward();
+
+		assertThat(ship.getLocation().getPoint().getX()).isEqualTo(22);
 	}
 }
